8344fbb12bc489eadbbcbc3fe8c26ea3fceebd84
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-04-21 20:37:35 +0300

    post data to mongodb

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,22 @@
-const register = (req, res, next) => {
-  res.status(200).json({ success: true });
+const User = require('../models/User');
+
+const register = async (req, res, next) => {
+  // POST DATA
+  const name = 'Ahmet Çetinkaya';
+  const email = 'ahmetcetinkaya0@outlook.com';
+  const password = '12346789';
+
+  // async await
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  res.status(200).json({ success: true, data: user });
 };
+
 module.exports = {
   register,
 };
