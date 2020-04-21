01e27e65de09d09cff121881ee48358a53ea6f5b
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-04-21 20:00:18 +0300

    mongodb connetion with mongoose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDatabase = require('./helpers/database/connectDatabase');
 const routers = require('./routers');
 
 // Enviroment Variables
 dotenv.config({ path: './config/env/config.env' }); // Oto config klasörüne bakar, özel olarak belirtmek gerekise.
+// MongoDB Connection
+connectDatabase();
+
 const PORT = process.env.PORT;
 const app = express();
 
