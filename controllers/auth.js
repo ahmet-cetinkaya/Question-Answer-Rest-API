dcc719b099bd5a1b3d91a0470645f90700429162
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-04-28 18:13:47 +0300

    express-async-handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,20 @@
 const User = require('../models/User');
 const CustomError = require('../helpers/error/CustomError');
-const register = async (req, res, next) => {
+const asyncErrorWrapper = require('express-async-handler');
+
+const register = asyncErrorWrapper(async (req, res, next) => {
   // POST DATA
   const name = 'Ahmet Çetinkaya';
   const email = 'ahmetcetinkaya0@outlook.com';
   const password = '1234';
 
-  // async await - try catch
-  try {
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
-  } catch (err) {
-    return next(err);
-  }
-
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
   res.status(200).json({ success: true, data: user });
-};
+});
 
 const errorTest = (req, res, next) => {
   return next(new TypeError('Bir Hata Oluştu'));
