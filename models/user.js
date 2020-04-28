fa8980702bf405e263fac05a7fda40c8d01e379b
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-04-28 19:51:46 +0300

    Password hash with bcrypt
    https://mongoosejs.com/docs/middleware.html#pre
    https://mongoosejs.com/docs/middleware.html#post

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -40,6 +41,20 @@ const UserSchema = new Schema({
     type: Boolean,
     default: false,
   },
+});
+
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) next();
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) next(err);
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model('User', UserSchema);
