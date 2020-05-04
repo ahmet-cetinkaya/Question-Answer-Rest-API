20d4c22d1dbca32ba0d1d7a801a93c3492b98ed2
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-05-04 16:30:00 +0300

    Get All Users

--- controllers/user.js
+++ controllers/user.js
@@ -4,10 +4,13 @@ const asyncErrorWrapper = require('express-async-handler');
 
 const getSingleUser = asyncErrorWrapper(async (req, res, next) => {
   const { id } = req.params;
-
   const user = await User.findById(id);
-
   return res.status(200).json({ success: true, data: user });
 });
 
-module.exports = { getSingleUser };
+const getAllUser = asyncErrorWrapper(async (req, res, next) => {
+  const users = await User.find();
+  return res.status(200).json({ success: true, data: users });
+});
+
+module.exports = { getSingleUser, getAllUser };
--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getSingleUser } = require('../controllers/user.js');
+const { getSingleUser, getAllUser } = require('../controllers/user.js');
 const { checkUserExists } = require('../middlewares/database/databaseErrorHelpers');
 
 const router = express.Router();
 
+router.get('/', getAllUser);
 router.get('/:id', checkUserExists, getSingleUser);
 
 module.exports = router;
