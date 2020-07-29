e3cf9a434f2e7b8772c72f4c019ed277b830fe8a
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-07-29 18:08:38 +0300

    Undo Like from Question

--- controllers/question.js
+++ controllers/question.js
@@ -62,6 +62,18 @@ const likeQuestion = asyncErrorWrapper(async (req, res, next) => {
   return res.status(200).json({ success: true, data: question });
 });
 
+const undoLikeQuestion = asyncErrorWrapper(async (req, res, next) => {
+  const { id } = req.params;
+  const question = await Question.findById(id);
+  if (!question.likes.includes(req.user.id)) {
+    return next(new CustomError('You cannot undo like operation for this question.', 400));
+  }
+  const index = question.likes.indexOf(req.user.id);
+  question.likes.splice(index, 1);
+  question.save();
+  return res.status(200).json({ success: true, data: question });
+});
+
 module.exports = {
   askNewQuestion,
   getAllQuestions,
@@ -69,4 +81,5 @@ module.exports = {
   editQuestion,
   deleteQuestion,
   likeQuestion,
+  undoLikeQuestion,
 };
--- routers/questions.js
+++ routers/questions.js
@@ -6,6 +6,7 @@ const {
   editQuestion,
   deleteQuestion,
   likeQuestion,
+  undoLikeQuestion,
 } = require('../controllers/question');
 const { getAccessToRoute, getQuestionOwnerAccess } = require('../middlewares/authorization/auth');
 const { checkQuestionExist } = require('../middlewares/database/databaseErrorHelpers');
@@ -14,6 +15,7 @@ const router = express.Router();
 router.get('/', getAllQuestions);
 router.get('/:id', checkQuestionExist, getSingleQuestion);
 router.get('/:id/like', [getAccessToRoute, checkQuestionExist], likeQuestion);
+router.get('/:id/undo_like', [getAccessToRoute, checkQuestionExist], undoLikeQuestion);
 router.post('/ask', getAccessToRoute, askNewQuestion);
 router.put(
   '/:id/edit',
