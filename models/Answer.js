8ff143fc86b60ee87cb5819af893e11e4b6383b2
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-08-08 14:41:25 +0300

    Answer Model

---
+++ models/Answer.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Questions = require('./Questions');
+const Schema = mongoose.Schema;
+
+const AnswerSchema = new Schema({
+  content: {
+    type: String,
+    required: [true, 'Please provide a content'],
+    minlength: [10, 'Please provide a content at least 10 characters'],
+  },
+  createdAt: { type: Date, default: Date.now },
+  likes: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  question: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Question',
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+AnswerSchema.pre('save', async function (next) {
+  if (!this.isModified('user')) return next();
+  try {
+    const question = await Questions.findById(this.question);
+    question.answers.push(this._id);
+    await question.save();
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = mongoose.model('Answer', AnswerSchema);
