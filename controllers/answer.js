c4203b68d65c6bb2e66f4a65f19ec2704087a15a
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-08-15 16:51:35 +0300

    Getting All Answers to a Question

--- controllers/answer.js
+++ controllers/answer.js
@@ -15,4 +15,13 @@ const addNewAnswerToQuestion = asyncErrorWrapper(async (req, res, next) => {
   return res.status(200).json({ success: true, data: answer });
 });
 
-module.exports = { addNewAnswerToQuestion };
+const getAllAnswerByQuestion = asyncErrorWrapper(async (req, res, next) => {
+  const { question_id } = req.params;
+
+  const question = await Questions.findById(question_id).populate('answers');
+  const answers = question.answers;
+
+  return res.status(200).json({ success: true, count: answers.length, data: answers });
+});
+
+module.exports = { addNewAnswerToQuestion, getAllAnswerByQuestion };
--- routers/answer.js
+++ routers/answer.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const { getAccessToRoute } = require('../middlewares/authorization/auth');
-const { addNewAnswerToQuestion } = require('../controllers/answer');
+const { addNewAnswerToQuestion, getAllAnswerByQuestion } = require('../controllers/answer');
 
 router.post('/', getAccessToRoute, addNewAnswerToQuestion);
+router.get('/', getAllAnswerByQuestion);
 
 module.exports = router;
