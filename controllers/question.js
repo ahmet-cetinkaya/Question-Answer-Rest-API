8bd079b3112fe770437b11ea4bdaaa4ab753b4f2
Ahmet Çetinkaya <53148314+ahmet-cetinkaya@users.noreply.github.com>
2020-08-19 18:32:28 +0300

    Pagination of Questions

--- controllers/question.js
+++ controllers/question.js
@@ -23,18 +23,43 @@ const getAllQuestions = async (req, res, next) => {
     select: 'name profile_image',
   };
 
+  // Search
   if (req.query.search) {
     const searchObject = {};
     const regex = new RegExp(req.query.search, 'i');
     searchObject['title'] = regex;
     query = query.where(searchObject);
   }
+
+  // Populate
   if (populate) query = query.populate(populateObject);
+
+  // Pagination
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 5;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const pagination = {};
+  const total = await Question.countDocuments();
+
+  if (startIndex > 0)
+    pagination.previous = {
+      page: page - 1,
+      limit: limit,
+    };
+
+  if (endIndex < total) pagination.next = { page: page + 1, limit: limit };
+
+  query = query.skip(startIndex).limit(limit);
 
   const questions = await query;
 
   return res.status(200).json({
     success: true,
+    count: questions.length,
+    pagination: pagination,
     data: questions,
   });
 };
